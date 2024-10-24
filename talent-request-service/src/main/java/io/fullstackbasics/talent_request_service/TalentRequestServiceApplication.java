a36a77b2ae49723c0fe3f1b52abd4742ce39594a
Shreya Ray <marsvsjupiter@gmail.com>
2024-10-24 15:46:00 +0530

    Incorporated API Gateway fixes Server port for multiple instances removed dependency on gateway-mvc to replace with just spring cloud gateway

--- talent-request-service/src/main/java/io/fullstackbasics/talent_request_service/TalentRequestServiceApplication.java
+++ talent-request-service/src/main/java/io/fullstackbasics/talent_request_service/TalentRequestServiceApplication.java
@@ -3,6 +3,7 @@ package io.fullstackbasics.talent_request_service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
